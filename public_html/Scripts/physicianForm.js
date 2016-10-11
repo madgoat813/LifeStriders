a96bfb4ba9e32d871335d1be403347b06c4380e8
Scott <Scott@ScottsComp>
2016-10-11 11:28:21 -0500

    Inserted try-catch around automation to catch errors gracefully.

--- public_html/Scripts/physicianForm.js
+++ public_html/Scripts/physicianForm.js
@@ -7,19 +7,23 @@ $(document).ready(function (e) {
      $('#printPreviewPhysicianFormOVER').hide();
 
      $('#printPreviewButton').click(function () {
-          var formfield = ["Client Name: " + $('#clientName').val(),"Birth Date: " + $('#birthDate').val(),"Address: " + $('#address').val(),
-          "Phone: " + $('#phone').val(),"Medical Diagnosis/Description of Disability: " + $('#medDiag').val(),
-          "Precautions/Contraindications: " + $('#preCon').val(),"Additional Comments: " + $('#addCom').val(),
-          "Physician's Name: " + $('#physName').val(),"Physician's Phone: " + $('#physPhone').val(),"Physician's Address: " + $('#physAddress').val(),
-          "Physician's Signature: " + $('#physSig').val(),"Todays Date: " + $('#todayDate').val()];
-          
+
           //This is not ideal, however I am begining to automate the process of creating the print preview screen
-          var printindex = ['#CN','#BD','#AD','#PH','#MD','#PC','#AC','#PN','#PP','#PD','#PS','#TD'];
-          
-          for(i = 0;i<formfield.length;i++){
-               $(printindex[i]).html(formfield[i]);
+          try {
+               var formfield = ["Client Name: " + $('#clientName').val(), "Birth Date: " + $('#birthDate').val(), "Address: " + $('#address').val(),
+                    "Phone: " + $('#phone').val(), "Medical Diagnosis/Description of Disability: " + $('#medDiag').val(),
+                    "Precautions/Contraindications: " + $('#preCon').val(), "Additional Comments: " + $('#addCom').val(),
+                    "Physician's Name: " + $('#physName').val(), "Physician's Phone: " + $('#physPhone').val(), "Physician's Address: " + $('#physAddress').val(),
+                    "Physician's Signature: " + $('#physSig').val(), "Todays Date: " + $('#todayDate').val()];
+               var printindex = ['#CN', '#BD', '#AD', '#PH', '#MD', '#PC', '#AC', '#PN', '#PP', '#PD', '#PS', '#TD'];
+
+               for (i = 0; i < formfield.length; i++) {
+                    $(printindex[i]).html(formfield[i]);
+               }
+          } catch (err) {
+               alert(err.message);
           }
-          
+
           $('#printPreviewPhysicianFormOVER').show();
           $('#formPhysicianForm').hide();
      });
@@ -32,7 +36,7 @@ $(document).ready(function (e) {
      });
 
 });
-    
-    
+
+
 
 
